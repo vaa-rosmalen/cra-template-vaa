dc058050cf37bb6a3f11372da270717ca8e4748c
Justin Steenhuis <justinsteenhuis@gmail.com>
2020-04-07 09:21:15 +0200

    Fix font usage

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "styles/global.scss";
+import "./index.scss";
 import { App } from "app";
 import * as serviceWorker from "./serviceWorker";
 import "./utilities/i18n";
