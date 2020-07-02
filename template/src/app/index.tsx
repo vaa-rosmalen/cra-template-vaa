c065316f978272df79b008dbd21f52207e054e8e
Justin Steenhuis <justinsteenhuis@gmail.com>
2020-07-02 13:24:36 +0200

    v1.8.0

--- template/src/app/index.tsx
+++ template/src/app/index.tsx
@@ -1,19 +1,19 @@
-import React, { Suspense } from "react";
-import "./styles.scss";
+import React, { Suspense } from 'react';
+import './styles.scss';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Home, Users } from "pages";
-import { Layout, ConfigProvider } from "antd";
-import { Header, ErrorBoundary } from "components";
-import { SWRConfig } from "swr";
-import { SWR_CONFIG } from "configs/swr-config";
-import nlNL from "antd/es/locale/nl_NL";
-import moment from "moment";
-import momentLocalization from "moment/locale/nl";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Home, Users } from 'pages';
+import { Layout, ConfigProvider } from 'antd';
+import { Header, ErrorBoundary } from 'components';
+import { SWRConfig } from 'swr';
+import { SWR_CONFIG } from 'configs/swr-config';
+import nlNL from 'antd/es/locale/nl_NL';
+// import moment from "moment";
+// import momentLocalization from "moment/locale/nl";
 
-moment.locale("nl", momentLocalization);
+// moment.locale("nl", momentLocalization);
 
 const { Content } = Layout;
 library.add(far);
@@ -25,7 +25,7 @@ export const App = () => {
         <ConfigProvider locale={nlNL}>
           <ErrorBoundary>
             <SWRConfig value={SWR_CONFIG}>
-              <Layout style={{ minHeight: "100vh" }}>
+              <Layout style={{ minHeight: '100vh' }}>
                 <Header />
                 <Content style={{ paddingTop: 80 }}>
                   <Suspense fallback={<p>loading</p>}>
